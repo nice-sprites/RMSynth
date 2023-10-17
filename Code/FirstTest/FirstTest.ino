10aa48f605fc95f1771cad2cd2e54be2e82e9b4e
DTz <126077368+nice-sprites@users.noreply.github.com>
2023-10-17 10:47:28 -0300

    Update FirstTest.ino

--- Code/FirstTest/FirstTest.ino
+++ Code/FirstTest/FirstTest.ino
@@ -1,12 +1,20 @@
-const int pinoC = 9;
-const int pinoD = 8;
-const int pinoE = 7; 
+const int pinoC = 22;
+const int pinoD = 23;
+const int pinoE = 24; 
+const int pinoF = 27;
+const int pinoG = 28;
+const int pinoA = 29; 
+const int pinoB = 30;
 const int pinoBuzzer = 10; 
 
 void setup() {
 pinMode(pinoC, INPUT_PULLUP);
 pinMode(pinoD, INPUT_PULLUP);
 pinMode(pinoE, INPUT_PULLUP);
+pinMode(pinoF, INPUT_PULLUP);
+pinMode(pinoG, INPUT_PULLUP);
+pinMode(pinoA, INPUT_PULLUP);
+pinMode(pinoB, INPUT_PULLUP);
 
 pinMode(pinoBuzzer, OUTPUT);
 digitalWrite(pinoBuzzer, LOW);
@@ -15,17 +23,35 @@ digitalWrite(pinoBuzzer, LOW);
 
 void loop(){
   if(digitalRead(pinoC) == LOW){
-      tone(pinoBuzzer, 278);
+      tone(pinoBuzzer, 261);
       delay(200);
   }
   
   else if(digitalRead(pinoD) == LOW){
-      tone(pinoBuzzer, 312);
+      tone(pinoBuzzer, 293);
       delay(200);
   }
 
   else if(digitalRead(pinoE) == LOW){
-      tone(pinoBuzzer, 350);
+      tone(pinoBuzzer, 329);
+      delay(200);
+  }
+   else if(digitalRead(pinoF) == LOW){
+      tone(pinoBuzzer, 349);
+      delay(200);
+  }
+
+  else if(digitalRead(pinoG) == LOW){
+      tone(pinoBuzzer, 392);
+      delay(200);
+  }
+   else if(digitalRead(pinoA) == LOW){
+      tone(pinoBuzzer, 440);
+      delay(200);
+  }
+
+  else if(digitalRead(pinoB) == LOW){
+      tone(pinoBuzzer, 493);
       delay(200);
   }
   else{
@@ -34,3 +60,4 @@ void loop(){
   }
   
 }
+
